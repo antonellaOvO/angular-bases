f774d2606d531e95e18fcf1ca7ddb4375443ec2c
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2024-09-02 11:16:43 +0200

    Creado Proyecto y Fin de Sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+/*Normalmente es recomendable los imports que se ordenaran de esta forma: */
+
+/*Aquí irían primero las importaciones de Javascript - en este caso no hay-*/
+
+/*Segundo las importaciones oficiales de Angular: */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/*Tercero: las importaciones de librerías de terceros: */
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+/*Por último, las importaciones de nuestro código: */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule,
+  ] /*En los imports siempre van a ir palabras tipo palabraModule*/,
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
